bedd94798ba59c30772bce3926258b72c4e01ce6
Évelyne Lachance <eslachance@gmail.com>
2018-03-03 10:23:33 -0500

    Initial working mongodb provider commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Enmap-Mongo
+
+Enmap-Mongo is a provider for the [Enmap](https://www.npmjs.com/package/enmap) module. 
+
+## Installation
+
+To install Enmap-BaseProvider simply run `npm i enmap-mongo`.
+
+## Usage
+
+```js
+// Load Enmap
+const Enmap = require('enmap');
+ 
+// Load EnmapMongo
+const EnmapMongo = require('enmap-mongo');
+ 
+// Initialize the provider
+const Provider = new EnmapMongo({ name: 'test' });
+ 
+// Initialize the Enmap with the provider instance.
+const myColl = new Enmap({ provider: provider });
+```
+
+Shorthand declaration: 
+
+```js
+const Enmap = require('enmap');
+const EnmapMongo = require('enmap-mongo');
+const myColl = new Enmap({ provider: new EnmapMongo({ name: 'test' }); });
+```
+
+## Options
+
+```js
+// Example with all options.
+const level = new EnmapMongo({ 
+  name: 'test',
+  collection: 'enmap',
+  user: 'username',
+  pass: 'password',
+  host: 'localhost',
+  port: 27017
+});
+```
+
+```js
+// Example with full URL.
+const level = new EnmapMongo({ 
+  name: 'test',
+  collection: 'enmap',
+  url: 'mongodb://username:password@localhost:27017/enmap'
+});
+```
+
+### name
+
+The `name` option is mandatory and defines the name of the table where the data is stored. 
+
+### collection
+
+The `collection` is optional and defines the collection where data is stored. If multiple enmap instances connect to the same collection, the same collection is used with different table names. The default collection is `enmap`.
+
+### host
+
+The `host` is optional and defines which host this module attempts to connect to. The default host is `localhost`.
+
+### port
+
+The `port` is optional and defines which port is used to connect to the Rethink DB. The default port is `27017`.
+
+### user, pass
+
+Optional, used for authentication. Needs both to work, or neither. 
+
+### url
+
+Optional. If used, the `host`, `port`, `user` and `pass` options are *ignored* , and `url` is used instead. Note that the `/collectionname` at the end of your url *must* correspond to the `collection` value (or `enmap`).
---
+++ src/index.js
@@ -0,0 +1,104 @@
+const { MongoClient } = require('mongodb');
+
+class EnmapMongo {
+
+  constructor(options) {
+    this.defer = new Promise((resolve) => {
+      this.ready = resolve;
+    });
+
+    if (!options.name) throw new Error('Must provide options.name');
+    this.name = options.name;
+
+    this.validateName();
+
+    this.auth = options.user && options.password ? `${options.user}:${options.password}@` : '';
+    this.dbName = options.dbName || 'enmap';
+    this.port = options.port || 27017;
+    this.host = options.host || 'localhost';
+
+    this.url = options.url || `mongodb://${this.auth}${this.host}:${this.port}`;
+  }
+
+  /**
+   * Internal method called on persistent Enmaps to load data from the underlying database.
+   * @param {Map} enmap In order to set data to the Enmap, one must be provided.
+   * @returns {Promise} Returns the defer promise to await the ready state.
+   */
+  async init(enmap) {
+    this.client = await MongoClient.connect(this.url);
+    this.db = this.client.db(this.dbName).collection(this.name);
+    const rows = await this.db.find({}).toArray();
+    for (const row of rows) {
+      enmap.set(row._id, row.value);
+    }
+    console.log(rows);
+    this.ready();
+    return this.defer;
+  }
+
+  /**
+   * Shuts down the underlying persistent enmap database.
+   */
+  close() {
+    this.client.close();
+  }
+
+  /**
+   * Set a value to the Enmap.
+   * @param {(string|number)} key Required. The key of the element to add to the EnMap object.
+   * If the EnMap is persistent this value MUST be a string or number.
+   * @param {*} val Required. The value of the element to add to the EnMap object.
+   * If the EnMap is persistent this value MUST be stringifiable as JSON.
+   */
+  set(key, val) {
+    if (!key || !['String', 'Number'].includes(key.constructor.name)) {
+      throw new Error('Keys should be strings or numbers.');
+    }
+    this.db.update({ _id: key }, { _id: key, value: val }, { upsert: true });
+  }
+
+  /**
+   * Asynchronously ensure a write to the Enmap.
+   * @param {(string|number)} key Required. The key of the element to add to the EnMap object.
+   * If the EnMap is persistent this value MUST be a string or number.
+   * @param {*} val Required. The value of the element to add to the EnMap object.
+   * If the EnMap is persistent this value MUST be stringifiable as JSON.
+   */
+  async setAsync(key, val) {
+    if (!key || !['String', 'Number'].includes(key.constructor.name)) {
+      throw new Error('Keys should be strings or numbers.');
+    }
+    await this.db.update({ _id: key }, { _id: key, value: val }, { upsert: true });
+  }
+
+  /**
+   * Delete an entry from the Enmap.
+   * @param {(string|number)} key Required. The key of the element to delete from the EnMap object.
+   * @param {boolean} bulk Internal property used by the purge method.
+   */
+  delete(key) {
+    this.db.remove({ _id: key }, { single: true });
+  }
+
+  /**
+   * Asynchronously ensure an entry deletion from the Enmap.
+   * @param {(string|number)} key Required. The key of the element to delete from the EnMap object.
+   * @param {boolean} bulk Internal property used by the purge method.
+   */
+  async deleteAsync(key) {
+    await this.db.remove({ _id: key }, { single: true });
+  }
+
+  /**
+   * Internal method used to validate persistent enmap names (valid Windows filenames)
+   * @private
+   */
+  validateName() {
+    // Do not delete this internal method.
+    this.name = this.name.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+  }
+
+}
+
+module.exports = EnmapMongo;
